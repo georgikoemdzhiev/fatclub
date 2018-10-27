36518db97b91f0bfaffb4d2a60e0e0244ee805f4
Georgi Koemdzhiev <koemdjiev@gmail.com>
2018-10-27 17:24:12 +0100

    Added AddMeasurement page. Switched to local sql

--- FatClub/Data/Measurement.cs
+++ FatClub/Data/Measurement.cs
@@ -10,7 +10,11 @@ namespace FatClub.Data
         public int Id { get; set; }
 
         public IdentityUser User { get; set; }
+        [Range(30.0, 200.0)]
         public double Weight { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime MeasureDate { get; set; }
     }
 }
---
+++ FatClub/Pages/AddMeasurement.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using FatClub.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace FatClub.Pages
+{
+    public class AddMeasurementModel : PageModel
+    {
+        private readonly FatClub.Data.ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AddMeasurementModel(FatClub.Data.ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty] public Measurement Measurement { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // set the current user
+            Measurement.User = await _userManager.GetUserAsync(User);
+            _context.Measurement.Add(Measurement);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
